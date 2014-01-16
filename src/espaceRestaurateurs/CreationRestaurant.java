cfb59cb186c3f2f7d89e20c69a9f771e5227973b
Eholie Solene <seholie@diabolo.enseeiht.fr>
2014-01-16 15:12:52 +0100

    iok

---
+++ src/espaceRestaurateurs/CreationRestaurant.java
@@ -0,0 +1,21 @@
+package espaceRestaurateurs;
+
+import javax.ejb.Stateful;
+
+@Stateful
+public class CreationRestaurant {
+	
+	Restaurant_itf resto;
+	
+	public void creerRestaurant(String nom, String tel, String voie, String ville, int nPlaces) {
+		resto = new Restaurant();
+		resto.setNom(nom);
+		resto.setTelephone(tel);
+		resto.set
+		resto.set
+		resto.set
+		resto.set
+		resto.set
+	}
+
+}
